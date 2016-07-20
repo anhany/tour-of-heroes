0db29901e2f1e1e2a26cc79ab13d3e8a8ee81632
anhany <Andrew.Hancey@centeva.com>
2016-07-20 10:19:04 -0600

    Finish multiple components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component'
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>My Heroes</h1>
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
           [class.selected]="hero === selectedHero" 
@@ -17,14 +15,7 @@ export class Hero {
         <!-- each hero goes here -->
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
     .selected {
@@ -74,13 +65,14 @@ export class Hero {
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
-
+  `],
+  directives: [HeroDetailComponent],
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
+
   selectedHero : Hero;
   
   onSelect(hero: Hero) { this.selectedHero = hero; }
